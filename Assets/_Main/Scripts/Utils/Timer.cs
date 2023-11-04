805a5407e9e1423ab16c237b1cca720f8a63f36b
LeshaTola <tola.lesha@mail.ru>
2023-11-04 12:48:09 +0300

    Upgrade Timer

--- Assets/_Main/Scripts/Utils/Timer.cs
+++ Assets/_Main/Scripts/Utils/Timer.cs
@@ -1,30 +1,41 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
 public class Timer
 {
-	private Action<float> timerStepAction;
-	private Action timerEndAction;
+	private Action<float> stepAction;
+	private Action endAction;
 
 	public float CurrentTime { get; private set; }
 	public float TargetTime { get; private set; }
 	public float TimeStep { get; private set; }
+	public bool Looping { get; private set; }
 
-	public Timer(float targetTime, float timeStep = 1f, Action<float> timerStepAction = null, Action timerEndAction = null)
+	public Timer
+		(float targetTime, float timeStep = 1f, bool looping = false, Action<float> stepAction = null, Action endAction = null)
 	{
 		TargetTime = targetTime;
 		TimeStep = timeStep;
-		this.timerStepAction = timerStepAction;
-		this.timerEndAction = timerEndAction;
+		Looping = looping;
+		this.stepAction = stepAction;
+		this.endAction = endAction;
 	}
 
-	public IEnumerator StartTimer()
+	public IEnumerator Start()
 	{
-		CurrentTime = 0;
-		while (CurrentTime < TargetTime)
+		do
 		{
-			yield return new WaitForSeconds(TimeStep);
-			CurrentTime += TimeStep;
-			timerStepAction?.Invoke(TimeStep);
-		}
+			CurrentTime = 0;
+			while (CurrentTime < TargetTime)
+			{
+				yield return new WaitForSeconds(TimeStep);
+				CurrentTime += TimeStep;
+				stepAction?.Invoke(TimeStep);
+			}
 
-		timerEndAction?.Invoke();
+			endAction?.Invoke();
+		}
+		while (Looping);
 	}
 }
