e8640fbaca69953d288eaa9f35b9f5b8b5ccda75
LeshaTola <tola.lesha@mail.ru>
2023-11-04 11:10:28 +0300

    Add Timer

---
+++ Assets/_Main/Scripts/Utils/Timer.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Timer
+{
+	private Action<float> timerStepAction;
+	private Action timerEndAction;
+
+	public Timer(float targetTime, float timeStep = 1f, Action<float> timerStepAction = null, Action timerEndAction = null)
+	{
+		TargetTime = targetTime;
+		TimeStep = timeStep;
+		this.timerStepAction = timerStepAction;
+		this.timerEndAction = timerEndAction;
+	}
+
+	public float CurrentTime { get; private set; }
+	public float TargetTime { get; private set; }
+	public float TimeStep { get; private set; }
+
+	public IEnumerator StartTimer()
+	{
+		CurrentTime = 0;
+		while (CurrentTime < TargetTime)
+		{
+			yield return new WaitForSeconds(TimeStep);
+			CurrentTime += TimeStep;
+			timerStepAction?.Invoke(TimeStep);
+		}
+
+		timerEndAction?.Invoke();
+	}
+}
